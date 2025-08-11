d281e24f699983e29ed92e84dded3c338f33a1dc
nandhan ideas2it.com <nandhan@ideas2it.com>
2025-08-11 20:08:36 +0530

    Cors update

--- backend/server.js
+++ backend/server.js
@@ -72,7 +72,7 @@ app.use('/api', limiter);
 // CORS
 console.log('CORS config:', config.cors);
 app.use(cors({
-  origin: 'http://localhost:3001',
+  origin: 'https://ims-git-main-nandhan-venkadesh-as-projects.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
